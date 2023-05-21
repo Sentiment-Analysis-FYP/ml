5b64997e9585a55d65fd66d3c71f6d34c628eb3d
Max Otuteye <maxotuteye12@gmail.com>
2023-05-21 18:57:27 +0000

    imported vader stuff

--- ml/apps.py
+++ ml/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
 import html
-import pathlib
+from pathlib import Path
 import os
 
+
+class WebAppConfig(AppConfig):
+    name = 'ml'
+    MODEL_PATH = Path("models")
+    BERT_PRETRAINED_PATH = Path("models/")
